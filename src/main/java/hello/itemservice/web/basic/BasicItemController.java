102f8687c054ec9eab181a5e3ff82c5bb6ac5003
oster <kms0843@naver.com>
2023-09-26 01:55:51 +0900

    상품 상세 상품 등록 @ModelAttribute 상품 수정 PRG Post/Redirect/Get RedirectAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,8 +5,8 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -18,11 +18,89 @@ public class BasicItemController {
 
     private final ItemRepository itemRepository;
 
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    } @RequiredArgsConstructor 를 사용하면 final이 있는 객체의 생성자를 만들어줌
+
     @GetMapping
     public String items(Model model){
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String username,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model){
+        Item item = new Item();
+        item.setItemName(username);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/items";
+    }
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item){
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); @ModelAttribute 에 들어가는 ("item") 이름으로 Model 을 자동 등록해줌
+        return "basic/items";
+    }
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item){
+//        이름을 생략 할 경유 객체이름의 첫글자를 소문자로 바꿔 model 을 등록한다 ex) HelloData item -> helloData item
+//        model.addAttribute("helloData", item);
+
+        itemRepository.save(item);
+        return "basic/items";
+    }
+//    @PostMapping("/add")
+    public String addItemV4(Item item){
+        itemRepository.save(item);
+        return "basic/items";
+    }
+//    @PostMapping("/add")
+    public String addItemV5(Item item){
+        itemRepository.save(item);
+        return "redirect:/basic/items/"+item.getId();
+    }
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item saveItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId",saveItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+        // redirect 에 들어가 있는 itemId로 치환 된다
+        // 들어가지 못한 status는 ?sataus=true 쿼리형식으로 뒤에 붙게 된다.
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+    @PostMapping("/{itemId}/edit")
+    public String update(@PathVariable long itemId, @ModelAttribute Item item){
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}"; //@PathVariable 있는 itemId로 치환 된다.
     }
 
     /*
